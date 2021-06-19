03bfa85adb6bfd114f2f395ec4d6a08efb48a766
Miguel Galves <mgalves@qipu.com.br>
2021-06-19 11:31:05 -0300

    Trata quedas do Twitter, com tempos de sleep crescentes

--- src/cmd_run_grabber.sh
+++ src/cmd_run_grabber.sh
@@ -4,8 +4,18 @@
 # author MIGUEL GALVES <mgalves@gmail.com>
 # SCRIPT QUE EXECUTA O LEITOR DE DADOS DO TWEITTER.
 #
+wait=1
+
 while true
 do
     echo "RUNNING GRABBER"
     python3 grabber.py
+    echo "GRABBER DOWN. SLEEPING..." $wait
+    sleep $wait
+    let wait=2*$wait
+    if [ $wait -eq 1024 ]
+    then
+        echo "Waited too much....stopping"
+        break
+    fi
 done
